dfc2b20e53299a7e2ab053170a9542b7ee28de23
Guillermo Vaamonde <guillevaamonde22@gmail.com>
2023-08-29 10:23:43 -0300

    ProyectoFinal

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,3 +1,7 @@
+/* CLASE8 */
+
+
+
 class Usuario {
     constructor(nombre, apellido, localidad, email) {
         this.nombre = nombre;
@@ -38,13 +42,94 @@ function agregarUsuarioALista(usuario) {
         <button class="eliminarBtn">Eliminar</button>`;
     
     nuevoUsuarioItem.querySelector(".eliminarBtn").addEventListener("click", () => {
-        nuevoUsuarioItem.remove(); // para eliminar usuarios
+        
+        Swal.fire({ /* MODAL PARA USUARIO ELIMINADO */
+        icon: "error",
+        title: "Estas seguro/a?",
+        showCancelButton: true,
+        confirmButtonText: "Si",
+        cancelButtonText: "No",})
+        .then((result) => {
+            if(result.isConfirmed){
+                nuevoUsuarioItem.remove();  // para eliminar usuarios
+            }
+        })
     });
-
+    Swal.fire({ /* MODAL PARA USUARIO AGREGADO */
+        icon: "success",
+        title: "Usuario creado",})
     listaUsuarios.appendChild(nuevoUsuarioItem);
+
 }
 
+const productosData = [
+    {
+        "id": 1,
+        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        "price": 109.95,
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "rating": {
+        "rate": 3.9,
+        "count": 120
+        }
+        },
+        {
+        "id": 2,
+        "title": "Mens Casual Premium Slim Fit T-Shirts ",
+        "price": 22.3,
+        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "rating": {
+        "rate": 4.1,
+        "count": 259
+        }
+        },
+        {
+        "id": 3,
+        "title": "Mens Cotton Jacket",
+        "price": 55.99,
+        "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+        "rating": {
+        "rate": 4.7,
+        "count": 500
+        }
+        },
+        {
+        "id": 4,
+        "title": "Mens Casual Slim Fit",
+        "price": 15.99,
+        "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+        "rating": {
+        "rate": 2.1,
+        "count": 430
+        }
+        },
+    // ... (rest of the products)
+];
+const productosDiv = document.getElementById("productos");
 
+productosData.forEach(producto => {
+    const productoDiv = document.createElement("div");
+    productoDiv.classList.add("producto");
+
+    productoDiv.innerHTML = `
+        <img src="${producto.image}" alt="${producto.title}">
+        <h3>${producto.title}</h3>
+        <p>${producto.description}</p>
+        <p>Price: $${producto.price}</p>
+        <p>Category: ${producto.category}</p>
+        <p>Rating: ${producto.rating.rate} (${producto.rating.count} reviews)</p>
+    `;
+
+    productosDiv.appendChild(productoDiv);
+});
 
 
 /* let nombre = document.getElementById("nombre")
